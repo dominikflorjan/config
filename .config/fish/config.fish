63481ff166567d336988936f00c6e2e6707dcdc0
Dominik Florjan <dominik.florjan@gmail.com>
2021-03-14 19:26:54 +0100

    Another day, another wasted time on config :')

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,10 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 source "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
 
-# Fish comment -- not the best way to add to path, shows prompt
-# fish_add_path /usr/local/texlive/2020/bin/x86_64-linux
-
+# Add to PATH 
 set -U fish_user_paths /usr/local/texlive/2020/bin/x86_64-linux $fish_user_paths
 
 ### Universal variables
 set -U EDITOR nvim
+set -x MANPAGER 'nvim +Man!'
+set -x MANWIDTH 999
