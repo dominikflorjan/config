19400456b02a336116be0a3b99812035976ac228
Dominik Florjan <dominik.florjan@gmail.com>
2021-06-15 08:21:36 +0200

    Reverted change audio to previous version

--- .config/i3/change_audio.sh
+++ .config/i3/change_audio.sh
@@ -2,22 +2,22 @@
 
 current=$(pacmd list-sinks | grep '\* index' | cut -c 12)
 
-sinks_count=$(pacmd list-sinks | grep -e name: | wc -l)
-
-((current=current+1))
-
-if [ $current -gt $sinks_count ]
-then 
-    $current = 1
-fi
-
-echo $current
-
-$(pacmd set-default-sink $current)
-
-# if [ $current == "1" ];
+# sinks_count=$(pacmd list-sinks | grep -e name: | wc -l)
+# 
+# ((current=current+1))
+# 
+# if [ $current -gt $sinks_count ]
 # then 
-    # $(pacmd set-default-sink 2)
-# else 
-    # $(pacmd set-default-sink 1)
+    # $current = 1
 # fi
+# 
+# echo $current
+# 
+# $(pacmd set-default-sink $current)
+
+if [ $current == "1" ];
+then 
+    $(pacmd set-default-sink 2)
+else 
+    $(pacmd set-default-sink 1)
+fi
