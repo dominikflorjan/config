b94d3a5eb018e18855391ded7a19c594599d38be
Dominik Florjan <dominik.florjan@gmail.com>
2024-02-14 08:49:26 +0100

    Updating lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,3 +1,5 @@
+-- Idk why but this has to be on the very top to work 
+vim.g.maplocalleader = ","
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
@@ -17,3 +19,20 @@ lvim.plugins = {
   config = function() require"lsp_signature".on_attach() end,
   },
 }
+
+vim.g.vimtex_compiler_latexmk = {
+    executable = 'latexmk',
+    aut_dir = 'aux',
+    out_dir = "out",
+}
+
+lvim.builtin.which_key.mappings["v"] = {
+  name = "Vimtex",
+  v = { "<cmd>:VimtexCompile<cr>", "Compile file" },
+  c = { "<cmd>:VimtexClean<cr>", "Clean" },
+}
+
+ls = require("luasnip")
+ls.config.set_config({
+  enable_autosnippets = true,
+})
